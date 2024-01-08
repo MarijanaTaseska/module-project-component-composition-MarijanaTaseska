90256b2338bba83010fedb801e65aa04203ae1f9
Marijana Taseska <marijana.taseska@gmail.com>
2024-01-07 20:02:01 -0800

    Second part of component somposition module project

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,42 @@
 import React from "react"
 import Figure from "./Figure"
+import styled,{keyframes} from "styled-components"
+
+const kf = keyframes`
+100%{
+  opacity: 1;
+  transform: scale(1) rotateZ(0);
+}
+`
+const StyledCard = styled.div`
+margin:1rem;
+padding: 1rem;
+border:3px solid purple;
+h1{
+  font-size:2rem;
+  text-decoration: solid underline purple 4px;
+  opacity: 0;
+  transform: scale(2) rotateZ(360deg);
+  animation:${kf} 0.5s ease-in-out forwards;
+};
+p{
+  padding:0.5rem;
+  &::first-line{
+font-style:italic;
+}
+};
+
+`
 
 export default function Card({title,text,imgURL,date}){
     return(
-      <div className='Card'>
+      <StyledCard className='Card'>
         <h1>{title}</h1>
         <p>{text}</p>  
       <Figure 
       imgURL={imgURL}
       caption ={date}
       />
-        </div>
+        </StyledCard>
     )
   }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,36 @@
 import React from "react"
+import styled,{keyframes} from "styled-components"
+const kf =keyframes`
+50%{
+  transform: scale(0.5);
+}
+100%{
+  transform:scale(1);
+  opacity: 1;
+}
+`
+const StyledFigure = styled.figure`
+width: 70%;
+img{
+  border: 5px solid purple;
+  &:hover{
+    transform: scale(1.2);
+  }
+}
+figcaption{
+text-align: center;
+font-style:italic;
+}
+transform: scale(2);
+opacity: 0;
+animation: ${kf} 0.3s ease-in-out forwards;
+`
 
 export default function Figure({imgURL,caption}){
     return (
-      <figure>
+      <StyledFigure>
       <img src={imgURL}/>
       <figcaption>This image was taken on {caption}</figcaption>
-      </figure>
+      </StyledFigure>
     )
   }
